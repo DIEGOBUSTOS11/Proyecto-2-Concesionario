8b8d13a99849361cf8edc0cfab42e78fede0207a
DIEGO <ferchobmg1107@gmail.com>
2019-02-23 08:58:34 -0500

    Creacion de proyecto consecionario

---
+++ ProyectoConsecionario/src/main/java/com/mycompany/proyectoconsecionario/Inicio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectoconsecionario;
+
+/**
+ *
+ * @author DiegoFernando
+ */
+public class Inicio {
+    
+     public static void main(String[] args) {
+        
+    }
+    
+    
+}
